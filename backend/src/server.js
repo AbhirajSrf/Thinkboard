389d45e42ad8fe85f6b20fc08af374f2afac77fa
Abhiraj Rai <raiayutra@gmail.com>
2025-12-27 21:55:43 +0545

    created HomePage and CreatePage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,6 +12,11 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 // middleware
+app.use(
+  cors({
+    origin: " http://localhost:5173",
+  })
+);
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,17 @@
-import React from "react";
 import { Route, Routes } from "react-router";
+
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailpage from "./pages/NoteDetailpage";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div data-theme="business">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button onClick={() => toast.error("congrats")}>click me</button>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-ful w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
-        <Route path="/" element={HomePage}></Route>
-        <Route path="/create" element={CreatePage}></Route>
-        <Route path="/note/:id" element={NoteDetailpage}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+        <Route path="/note/:id" element={<NoteDetailpage />}></Route>
       </Routes>
     </div>
   );
